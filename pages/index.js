f81fa982d566fca077696632e6d43896d16e0fb2
AbdulWahabRaza123 <abdulwahabraza123@gmail.com>
2022-12-16 13:07:49 +0500

    express with next

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 const index = () => {
+  const Fun = async () => {
+    const res = await fetch("/api/success");
+    const data = await res.json();
+    console.log("This is response from backend ", data);
+  };
+  useEffect(() => {
+    Fun();
+  }, []);
   return <div>This is frontend and backend</div>;
 };
-
 export default index;
--- server/Routes/success.js
+++ server/Routes/success.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 router.get("/success", (req, res) => {
   console.log("Hello Success");
-  return res.end("We dare it");
+  return res.json("We dare it");
 });
 router.get("/error", (req, res) => {
-  return res.end("We can't dare it");
+  return res.json("We can't dare it");
 });
 module.exports = router;
